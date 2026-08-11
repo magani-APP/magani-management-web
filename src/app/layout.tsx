08100b2260dfdabc17f7908b8e85e98bf66f2212
Ted Mbang <tdark237@gmail.com>
2026-08-11 16:35:03 +0100

    chore: initial commit - PharmaOs setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "PharmaOS",
+    description: "Système de gestion de pharmacie",
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="fr">
+            <body>{children}</body>
+        </html>
+    );
+}
