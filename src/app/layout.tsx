9dc254c3fd88372565a3e485e36fe4aec1153036
AJOUMOU <juniorajoumou@gmail.com>
2026-08-28 12:37:57 +0100

    title correction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "Magali",
+    title: "Magani officine",
     description: "Système de gestion de pharmacie",
 };
 
